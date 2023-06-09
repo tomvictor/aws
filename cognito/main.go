bb35531cfcf1d001ef2f4ec8aabf8114863d4bdd
Tom Victor <vjtomvictor@icloud.com>
2023-06-10 00:29:07 +0200

    add cognito user description

--- cognito/main.go
+++ cognito/main.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"github.com/aws/aws-sdk-go-v2/config"
 	"github.com/aws/aws-sdk-go-v2/service/cognitoidentityprovider"
+	"github.com/aws/aws-sdk-go-v2/service/cognitoidentityprovider/types"
 	"log"
 )
 
@@ -12,13 +13,34 @@ func main() {
 	fmt.Println("List aws cognito pools")
 
 	ctx := context.Background()
-
 	defaultConfig, err := config.LoadDefaultConfig(ctx, config.WithRegion("eu-north-1"))
 	if err != nil {
 		log.Fatal(err.Error())
 	}
 
 	client := cognitoidentityprovider.NewFromConfig(defaultConfig)
+	firstPool := DescribeCognitoUserPools(err, client, ctx)
+	DescribeUsers(firstPool, err, client, ctx)
+
+}
+
+func DescribeUsers(firstPool types.UserPoolDescriptionType, err error, client *cognitoidentityprovider.Client, ctx context.Context) {
+	userInput := &cognitoidentityprovider.ListUsersInput{
+		UserPoolId: firstPool.Id,
+	}
+	users, err := client.ListUsers(ctx, userInput)
+
+	fmt.Println(len(users.Users))
+
+	for _, user := range users.Users {
+		fmt.Println(*user.Username)
+		for _, att := range user.Attributes {
+			fmt.Println(*att.Name, *att.Value)
+		}
+	}
+}
+
+func DescribeCognitoUserPools(err error, client *cognitoidentityprovider.Client, ctx context.Context) types.UserPoolDescriptionType {
 	userPoolInput := &cognitoidentityprovider.ListUserPoolsInput{
 		MaxResults: 10,
 	}
@@ -31,4 +53,5 @@ func main() {
 		fmt.Println(*pool.Name)
 	}
 
+	return pools.UserPools[0]
 }
